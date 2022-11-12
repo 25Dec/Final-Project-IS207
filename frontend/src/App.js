0135858c792094a57f24cf3599d2300eb29074f1
25Dec <thiennhannguyenhuynh@gmail.com>
2022-11-12 22:00:43 +0700

    Add RequireAuth component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginSection from "./pages/login/LoginSection";
+import WebAdmin from "./pages/web-admin/WebAdmin";
+import WebGuest from "./pages/web-guest/WebGuest";
+import RequireAuth from "./components/RequireAuth";
 
 const App = () => {
 	return (
-		<div id="App">
-			<LoginSection />
-		</div>
+		<Routes>
+			<Route path="/">
+				{/* Public routes */}
+				<Route path="login" element={<LoginSection />} />
+
+				{/* Protected routes */}
+				<Route element={<RequireAuth />}>
+					<Route path="/admin" element={<WebGuest />} />
+					<Route path="/guest" element={<WebGuest />} />
+				</Route>
+			</Route>
+		</Routes>
 	);
 };
 
